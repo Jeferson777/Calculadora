c5a0e87f3b024e7556c27cbb122744fd305a0f6f
Jeferson Soaris <g7harmonic@outlook.com>
2021-03-05 05:33:14 -0300

    # Criado uma classe

---
+++ calculos/classCalculo.py
@@ -0,0 +1,36 @@
+"""
+Pacote contendo uma Classe chamada Calculo com métodos de aplicação.
+"""
+
+
+class Calculos:
+    # Iniciando a Classe e instanciando parâmetros:
+    def __init__(self, num1, operador, num2):
+        self.num1 = num1
+        self.operador = operador
+        self.num2 = num2
+
+    # Métodos()
+    def somar(self):
+        return self.num1 + self.num2
+
+    def subtrair(self):
+        return self.num1 - self.num2
+
+    def multiplicar(self):
+        return self.num1 * self.num2
+
+    def dividir(self):
+        return self.num1 / self.num2
+
+    def quocienteDivisao(self):
+        return self.num1 // self.num2
+
+    def restoDivisao(self):
+        return self.num1 % self.num2
+
+    def potenciacao(self):
+        return self.num1 ** self.num2
+
+
+

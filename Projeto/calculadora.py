e838a6fbc76a868d34c9802a8d52556fcd83fb95
Jeferson Soaris <g7harmonic@outlook.com>
2021-03-05 05:32:30 -0300

    # Gerado arquivo executável

---
+++ Projeto/calculadora.py
@@ -0,0 +1,30 @@
+"""
+Aplicação Calculadora.
+"""
+
+# Importando um módulo com uma Classe do pacote calculo:
+from calculos.classCalculo import Calculos
+
+# Recebendo entrada de dados:
+a = int(input('Insira um número: '))
+operador = input('Insira um operador: ')
+b = int(input('Insira outro número: '))
+
+# Instanciando a "Classe" e inserindo as variáveis com os dados de parâmetros:
+soma = Calculos(a, operador, b)
+
+# Condicional com métodos da classe:
+if operador == '+':
+    print(Calculos.somar(soma))  # Inserindo a variável instanciada.
+elif operador == '-':
+    print(Calculos.subtrair(soma))
+elif operador == '*':
+    print(Calculos.multiplicar(soma))
+elif operador == '/':
+    print(Calculos.dividir(soma))
+elif operador == '//':
+    print(Calculos.quocienteDivisao(soma))
+elif operador == '%':
+    print(Calculos.restoDivisao(soma))
+else:
+    print(Calculos.potenciacao(soma))

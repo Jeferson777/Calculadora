6c9075266ed22c134ae7bfdbcd1a04d5d22e7c8b
Jeferson Soaris <g7harmonic@outlook.com>
2021-04-01 03:03:31 -0300

    Alterado nome de pastas

---
+++ package.py/calculadora.py
@@ -0,0 +1,33 @@
+"""
+Aplicação Calculadora.
+"""
+
+# Importando um módulo com uma Classe do pacote calculo:
+from calculos.classCalculo import Calculos
+# from calculos.classCalculo import Calculos
+
+# Recebendo entrada de dados:
+a = int(input('Insira um número: '))
+operador = input('Insira um operador: ')
+b = int(input('Insira outro número: '))
+
+# Instanciando a "Classe" e inserindo as variáveis com os dados de parâmetros:
+soma = Calculos(a, operador, b)
+
+# Condicional com métodos da classe:
+if operador == '+':
+    print(Calculos.somar(soma))  # Inserindo a variável instanciada.
+elif operador == '-':
+    print(Calculos.subtrair(soma))
+elif operador == '*':
+    print(Calculos.multiplicar(soma))
+elif operador == '/':
+    print(Calculos.dividir(soma))
+elif operador == '//':
+    print(Calculos.quociente(soma))
+elif operador == '%':
+    print(Calculos.resto(soma))
+elif operador == '**':
+    print(Calculos.potenciacao(soma))
+else:
+    print("Operador incorreto!! (+,-,*,/,//,%,**)")
